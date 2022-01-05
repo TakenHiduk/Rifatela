ca86b7815750c2aa2e036909349d9292a95507ff
Carlos Daniel Garfio Murillo <cgarfiomurillo@outlook.com>
2022-01-05 04:43:42 -0700

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  plugins: ['svelte3', '@typescript-eslint'],
+  ignorePatterns: ['src/**/*.d.ts'],
+  overrides: [
+    {
+      files: ['*.svelte', '*.ts', '*.tsx'],
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+        extraFileExtensions: ['.svelte'],
+      },
+      processor: 'svelte3/svelte3',
+    },
+  ],
+  settings: {
+    'svelte3/typescript': () => require('typescript'),
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  rules:{
+    'no-console':process.env.NODE_ENV === 'production'?'error':'warn',
+    'no-debugger':process.env.NODE_ENV === 'production'?'error':'warn'
+  }
+};
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<style lang="scss">
+</style>
+
+<script context="module">
+</script>
+
+<script lang="ts">
+  export let name = 'test';
+</script>
+
+<h1>Welcome to SvelteKit {name}</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
